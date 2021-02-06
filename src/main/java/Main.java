a04ca2262338babd1460fe0a85ed1ec215595a54
WandererZhang <zzp924391@gmail.com>
2021-02-07 02:49:18 +0800

    add MysqlServer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 import protocol.http.*;
 import lombok.extern.slf4j.Slf4j;
+import protocol.mysql.MysqlServer;
 import protocol.redis.RedisServer;
 import protocol.telnet.TelnetServer;
 
@@ -19,6 +20,7 @@ public class Main {
     private static final int HTTP_PORT = 8080;
     private static final int TELNET_PORT = 23;
     private static final int REDIS_PORT = 6379;
+    private static final int MYSQL_PORT = 3306;
 
     public static void main(String[] args) {
         BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(CPU_NUM);
@@ -28,6 +30,7 @@ public class Main {
         HttpServer httpServer = new HttpServer(HTTP_PORT);
         TelnetServer telnetServer = new TelnetServer(TELNET_PORT);
         RedisServer redisServer = new RedisServer(REDIS_PORT);
+        MysqlServer mysqlServer = new MysqlServer(MYSQL_PORT);
         while (true) {
             //TODO 通过MQTT推送消息控制蜜罐启动
             Scanner scanner = new Scanner(System.in);
@@ -49,6 +52,12 @@ public class Main {
             }
             if (str.equals("6")){
                 redisServer.closeServer();
+            }
+            if (str.equals("7")){
+                serverThreadPool.execute(mysqlServer);
+            }
+            if (str.equals("8")){
+                mysqlServer.closeServer();
             }
         }
     }
---
+++ src/main/java/protocol/mysql/MysqlServer.java
@@ -0,0 +1,61 @@
+package protocol.mysql;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Mysql服务器
+ *
+ * @author 78445
+ */
+public class MysqlServer implements Runnable {
+    private final int port;
+    private static Channel mysqlChannel;
+    private static final Logger logger = LoggerFactory.getLogger(MysqlServer.class);
+
+    public MysqlServer(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 128);
+            b.childOption(ChannelOption.TCP_NODELAY, true);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new MysqlServerInitializer());
+            ChannelFuture f = b.bind(port).sync();
+            logger.info("Netty protocol.mysql server listening on port " + port);
+            mysqlChannel = f.channel();
+            mysqlChannel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public void closeServer() {
+        if (mysqlChannel != null) {
+            logger.info("close MysqlServer");
+            mysqlChannel.close();
+            mysqlChannel = null;
+        }
+    }
+}
